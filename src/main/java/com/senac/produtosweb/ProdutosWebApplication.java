482f6c775f0452ceabba9a1d2345536dc56a1c40
Alysson Oliveira <3oalysson@gmail.com>
2023-11-13 18:15:06 -0300

    Inicial

---
+++ src/main/java/com/senac/produtosweb/ProdutosWebApplication.java
@@ -0,0 +1,13 @@
+package com.senac.produtosweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutosWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProdutosWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/senac/produtosweb/ProdutosWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.produtosweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
